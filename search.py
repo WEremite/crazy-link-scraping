177593b5352a1f65074172f4e2a5640bb49595b9
weremite <weremite@gmail.com>
2022-04-28 19:00:17 +0300

    Add functionality to open two different web browsers

--- search.py
+++ search.py
@@ -2,20 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import click
+import webbrowser
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.firefox.service import Service
+# from webdriver_manager.firefox import GeckoDriverManager
+#
+# # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+# # driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 
 @click.command()
 @click.option('-s', '--search', "user_query", default='World', help="User's search query")
-@click.option('-b', '--browser', "browser", default='Chrome', help="Browser to use")
+@click.option('-b', '--browser', "browser", default='chrome', help="Browser to use")
 def main(user_query, browser):
     print(f"Searching for {user_query} in {browser}...")
 
     replace_whitespace = user_query.replace(' ', '+')
     number_of_search_results = 10
     url = f"https://www.google.com/search?q={replace_whitespace}&num={number_of_search_results}"
-
-    requests_results = requests.get(url)
-    soup_link = BeautifulSoup(requests_results.content, "html.parser")
+    webbrowser.get(browser).open(url)
+    requests_result = requests.get(url)
+    soup_link = BeautifulSoup(requests_result.content, "html.parser")
     links = soup_link.find_all("a")
 
     result = open("results.txt", "w")
