a38e0406d947bede127373b143bbace21432d8dd
weremite <weremite@gmail.com>
2022-04-30 14:59:44 +0300

    Add output option

--- search.py
+++ search.py
@@ -1,22 +1,17 @@
 import requests
-from bs4 import BeautifulSoup
-import os
 import click
 import webbrowser
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.firefox.service import Service
-# from webdriver_manager.firefox import GeckoDriverManager
-#
-# # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-# # driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+import pandas
+import os
+from bs4 import BeautifulSoup
+from pathlib import Path
 
 
 @click.command()
 @click.option('-s', '--search', "user_query", default='World', help="User's search query")
 @click.option('-b', '--browser', "browser", default='chrome', help="Browser to use")
-def main(user_query, browser):
+@click.option('-o', '--output', "output", default='not', help="File to save the results")
+def main(user_query, browser, output):
     print(f"Searching for {user_query} in {browser}...")
 
     replace_whitespace = user_query.replace(' ', '+')
@@ -27,24 +22,46 @@ def main(user_query, browser):
     soup_link = BeautifulSoup(requests_result.content, "html.parser")
     links = soup_link.find_all("a")
 
-    result = open("results.txt", "w")
+    my_file = Path(output)
 
-    print("Pushing results in file -> results.txt...")
+    if my_file.exists():
+        to_file(links, my_file)
+    else:
+        to_console(links)
 
+
+def to_file(links, my_file):
+    result = open(my_file, "w")
+    result.write("Title,URL\n")
+
+    add_results(links, result)
+
+    print("Done!")
+
+
+def to_console(links):
+    result = open('result.csv', "w")
+    result.write("Title,URL\n")
+
+    add_results(links, result)
+
+    answer = pandas.read_csv('result.csv')
+    print(answer)
+
+    os.remove('result.csv')
+
+    print("Done!")
+
+
+def add_results(links, result):
     for link in links:
         link_href = link.get('href')
         if "url?q=" in link_href and "webcache" not in link_href:
             title = link.find_all('h3')
             if len(title) > 0:
-                result.write(title[0].getText() + "\n")  # write title
-                result.write(link.get('href').split("?q=")[1].split("&sa=U")[0] + "\n\n")  # write link
-
+                result.write(title[0].getText().replace(",", "") + ",")  # write title
+                result.write(link.get('href').split("?q=")[1].split("&sa=U")[0] + "\n")  # write link
     result.close()
-
-    if os.path.getsize("results.txt") == 0:
-        print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-
-    print("Done!")
 
 
 main()
