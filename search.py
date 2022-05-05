e77f70c43ad8cddc21318c10c88efa2da21d83e6
weremite <weremite@gmail.com>
2022-05-05 22:15:23 +0300

    Add implementation of saving results to file and output to console

--- search.py
+++ search.py
@@ -1,12 +1,12 @@
-import requests
 import click
-import webbrowser
 import pandas
-import os
+import time
 from bs4 import BeautifulSoup
+from bs4.element import Tag
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 @click.command()
@@ -16,70 +16,58 @@ from selenium.webdriver.common.keys import Keys
 def main(user_query, browser, output):
     print(f"Searching for {user_query} in {browser}...")
     if browser.capitalize() == 'Firefox':
+        firefox_capabilities = DesiredCapabilities.FIREFOX
+        firefox_capabilities['marionette'] = False
         driver = webdriver.Firefox(executable_path="./drivers/geckodriver")
     elif browser.capitalize() == 'Edge':
         driver = webdriver.Edge(executable_path="./drivers/msedgedriver")
     else:
-        driver = webdriver.Chrome("./drivers/chromedriver")
+        driver = webdriver.Chrome(executable_path="./drivers/chromedriver")
 
     driver.get("https://www.google.com")
-    print(driver.title)
+
     search_bar = driver.find_element(by="name", value="q")
     search_bar.clear()
     search_bar.send_keys(user_query)
     search_bar.send_keys(Keys.RETURN)
-    print(driver.current_url)
-    driver.close()
+
+    time.sleep(3)
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    result_div = soup.find_all('div', attrs={'class': 'g'})
+
+    links = []
+    titles = []
+
+    for result in result_div:
+        link = result.find('a', href=True)
+        title = result.find('h3')
+
+        if isinstance(title, Tag):
+            title = title.get_text()
+
+        if link != '' and link['href'][0] != "/" and title != '':
+            links.append(link['href'])
+            titles.append(title)
+
+    my_file = Path(output)
+
+    if my_file.exists():
+        to_file(links, titles, my_file)
+    else:
+        to_console(links, titles)
 
 
-#     replace_whitespace = user_query.replace(' ', '+')
-#     number_of_search_results = 10
-#     url = f"https://www.google.com/search?q={replace_whitespace}&num={number_of_search_results}"
-#     webbrowser.get(browser).open(url)
-#     requests_result = requests.get(url)
-#     soup_link = BeautifulSoup(requests_result.content, "html.parser")
-#     links = soup_link.find_all("a")
-#
-#     my_file = Path(output)
-#
-#     if my_file.exists():
-#         to_file(links, my_file)
-#     else:
-#         to_console(links)
-#
-#
-# def to_file(links, my_file):
-#     result = open(my_file, "w")
-#     result.write("Title,URL\n")
-#
-#     add_results(links, result)
-#
-#     print("Done!")
-#
-#
-# def to_console(links):
-#     result = open('result.csv', "w")
-#     result.write("Title,URL\n")
-#
-#     add_results(links, result)
-#
-#     answer = pandas.read_csv('result.csv')
-#     print(answer)
-#
-#     os.remove('result.csv')
-#
-#     print("Done!")
-#
-#
-# def add_results(links, result):
-#     for link in links:
-#         link_href = link.get('href')
-#         if "url?q=" in link_href and "webcache" not in link_href:
-#             title = link.find_all('h3')
-#             if len(title) > 0:
-#                 result.write(title[0].getText().replace(",", "") + ",")  # write title
-#                 result.write(link.get('href').split("?q=")[1].split("&sa=U")[0] + "\n")  # write link
-#     result.close()
+def to_console(links, titles):
+    for (link, title) in zip(links[:10], titles):
+        print(f"{title} \n - {link} \n ------------------------------")
+
+
+def to_file(links, titles, my_file):
+    df = pandas.DataFrame({"Title": titles[:10], "Link": links[:10]})
+    df.to_csv(my_file, index=False)
+
+    print(pandas.read_csv(my_file))
 
 
 main()
