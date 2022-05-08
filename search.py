35de1cf279305a09999200722e3ec8b1cee3ae49
weremite <weremite@gmail.com>
2022-05-08 15:43:43 +0300

    Refactor code

--- search.py
+++ search.py
@@ -18,59 +18,86 @@ from selenium.webdriver.edge.service import Service
 def main(user_query, browser, output):
     print(f"Searching for {user_query} in {browser}...")
 
-    driver = Browser(browser).get_driver()
-
-    driver.get("https://www.google.com")
-
-    search_bar = driver.find_element(by="name", value="q")
-    search_bar.clear()
-    search_bar.send_keys(user_query)
-    search_bar.send_keys(Keys.RETURN)
-
-    time.sleep(3)
-
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    result_div = soup.find_all('div', attrs={'class': 'g'})
-
-    links = []
-    titles = []
-
-    for result in result_div:
-        link = result.find('a', href=True)
-        title = result.find('h3')
-
-        if isinstance(title, Tag):
-            title = title.get_text()
-
-        if link != '' and link['href'][0] != "/" and title != '':
-            links.append(link['href'])
-            titles.append(title)
-
-    my_file = Path(output)
-
-    if my_file.exists():
-        to_file(links, titles, my_file)
-    else:
-        to_console(links, titles)
+    Output(user_query, output)
 
 
-def to_console(links, titles):
-    for (link, title) in zip(links[:10], titles):
-        print(f"{title} \n - {link} \n ------------------------------")
+class Output:
+    def __init__(self, user_query, output):
+        self.output_file = Path(output)
+        self.result = Results(user_query, output).get_results()
+        self.links = self.result[0]
+        self.titles = self.result[1]
+
+        if self.output_file.exists():
+            self.to_file()
+        else:
+            self.to_console()
+
+    def to_file(self):
+        df = pandas.DataFrame({"Title": self.titles[:10], "Link": self.links[:10]})
+        df.to_csv(self.output_file, index=False)
+
+        print(pandas.read_csv(self.output_file))
+
+    def to_console(self):
+        for (link, title) in zip(self.links[:10], self.titles):
+            print(f"{title} \n - {link} \n ------------------------------")
 
 
-def to_file(links, titles, my_file):
-    df = pandas.DataFrame({"Title": titles[:10], "Link": links[:10]})
-    df.to_csv(my_file, index=False)
+class Results:
+    def __init__(self, user_query, output):
+        self.result_div = Search(user_query, output).get_div()
+        self.links = []
+        self.titles = []
 
-    print(pandas.read_csv(my_file))
+        self.save_results()
+
+    def save_results(self):
+        for result in self.result_div:
+            link = result.find('a', href=True)
+            title = result.find('h3')
+
+            if isinstance(title, Tag):
+                title = title.get_text()
+
+            if link != '' and link['href'][0] != "/" and title != '':
+                self.links.append(link['href'])
+                self.titles.append(title)
+
+    def get_results(self):
+        return self.links, self.titles
+
+
+class Search:
+    def __init__(self, user_query, browser):
+        self.user_query = user_query
+        self.driver = Browser(browser).get_driver()
+        self.result_div = None
+
+        self.searching()
+
+    def searching(self):
+        self.driver.get("https://www.google.com")
+
+        search_bar = self.driver.find_element(by="name", value="q")
+        search_bar.clear()
+        search_bar.send_keys(self.user_query)
+        search_bar.send_keys(Keys.RETURN)
+
+        time.sleep(3)
+
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
+        self.result_div = soup.find_all('div', attrs={'class': 'g'})
+
+    def get_div(self):
+        return self.result_div
 
 
 class Browser:
     def __init__(self, browser):
         self.driver = None
         self.browser = browser.capitalize()
-        
+
         self.set_driver()
 
     def set_driver(self):
@@ -88,4 +115,5 @@ class Browser:
         return self.driver
 
 
-main()
+if __name__ == "__main__":
+    main()
